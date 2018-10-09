116786733d8dfc3325e6f2867f84a71b55e66ab4
tiandeyang <tiandeyang52@163.com>
2018-10-09 16:16:44 +0800

    bigDecimal 比较大小

--- src/main/java/com/dytian/utils/BigDecimals.java
+++ src/main/java/com/dytian/utils/BigDecimals.java
@@ -3,43 +3,66 @@ package com.dytian.utils;
 
 import java.math.BigDecimal;
 
-public class BigDecimals {
+public class BigDecimals<E> {
+
 
     /**
-     *
      * @param a
      * @param pattern >,<,=,>=,<=
      * @param b
      * @return
      */
-    public static boolean compareTo(BigDecimal a,String pattern,BigDecimal b){
+    public boolean compareTo(E a, String pattern, E b) {
 
-        if (pattern.equals(">")){
-            return a.compareTo(b) > 0;
+        BigDecimal valA = BigDecimal.ZERO;
+        BigDecimal valB = BigDecimal.ZERO;
+
+        if (a instanceof Integer) {
+            System.out.println("aaa");
+            valA = valA.add(new BigDecimal((Integer) a));
+            valB = valB.add(new BigDecimal((Integer) b));
         }
-        if (pattern.equals(">=")){
-            return a.compareTo(b) >= 0;
+
+        if (a instanceof BigDecimal) {
+            valA = valA.add((BigDecimal) a);
+            valB = valB.add((BigDecimal) b);
         }
-        if (pattern.equals("<")){
-            return a.compareTo(b) < 0;
+
+        if (pattern.equals(">")) {
+            return valA.compareTo(valB) > 0;
         }
-        if (pattern.equals("<=")){
-            return a.compareTo(b) <= 0;
+        if (pattern.equals(">=")) {
+            return valA.compareTo(valB) >= 0;
         }
-        if (pattern.equals("=")){
-            return a.compareTo(b) == 0;
+        if (pattern.equals("<")) {
+            return valA.compareTo(valB) < 0;
         }
-        throw new IllegalArgumentException(pattern+" 参数非法");
+        if (pattern.equals("<=")) {
+            return valA.compareTo(valB) <= 0;
+        }
+        if (pattern.equals("=")) {
+            return valA.compareTo(valB) == 0;
+        }
+        throw new IllegalArgumentException(pattern + " 参数非法");
+    }
+
+    public static BigDecimals Instance() {
+        return new BigDecimals();
     }
 
 
     public static void main(String[] args) {
 
-        BigDecimal a = new BigDecimal("20.23");
+        BigDecimal a = new BigDecimal("20.");
         BigDecimal b = new BigDecimal("20.2");
+        Integer a1 = 100;
+        Integer a2 = 100;
+      //  boolean b2 = BigDecimals.Instance().compareTo(a, "<=", b);
 
-        boolean b1 = BigDecimals.compareTo(b, "**", a);
-        System.out.println(b1);
+        boolean b3 = BigDecimals.Instance().compareTo(a1, "<", a2);
+
+   //     System.out.println(b2);
+        System.out.println(b3);
 
     }
 
