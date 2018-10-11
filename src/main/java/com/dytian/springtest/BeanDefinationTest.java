925c6445d2a690fceaff9c7c306265391ccbf886
tiandeyang <tiandeyang52@163.com>
2018-10-11 22:02:54 +0800

    BufferTest

---
+++ src/main/java/com/dytian/springtest/BeanDefinationTest.java
@@ -0,0 +1,33 @@
+package com.dytian.springtest;
+
+import com.dytian.testcode.StrStr;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+
+public class BeanDefinationTest {
+
+
+    public void defineBean() {
+
+        DefaultListableBeanFactory beanRegistry = new DefaultListableBeanFactory();
+        AbstractBeanDefinition defination = new RootBeanDefinition(StrStr.class);
+
+        beanRegistry.registerBeanDefinition("beanName",defination);
+        BeanFactory beanFactory =  beanRegistry;
+        StrStr str = (StrStr) beanFactory.getBean("beanName");
+        int i = str.strStr("asdfasdf", "as");
+        System.out.println(i);
+
+    }
+
+    public static void main(String[] args) {
+
+        BeanDefinationTest beanDefinationTest = new BeanDefinationTest();
+        beanDefinationTest.defineBean();
+
+    }
+
+
+}
