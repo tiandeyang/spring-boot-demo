6e2fdc1441115542cf0a714894f882fc4c7300fc
tiandeyang <tiandeyang52@163.com>
2018-11-26 11:02:47 +0800

    file upload test and freemarker test

---
+++ src/main/java/com/dytian/file/Files.java
@@ -0,0 +1,21 @@
+package com.dytian.file;
+
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+public class Files {
+
+    public static void main(String[] args) throws IOException {
+        File fileDectory = new File("D:\\阅米UI\\纤姿洁\\花舞飞扬系列\\1本色通用详情\\");
+        Collection<File> files = FileUtils.listFiles(fileDectory, null, false);
+        for (File file:files){
+        //    FileUtils.moveFileToDirectory(file,new File("D:\\test"),true);
+         //   FileUtils.copyFileToDirectory(file,new File("D:\\test"),true);
+            file.renameTo(new File("D:\\test\\rename"+file.getName()));
+        }
+    }
+}
