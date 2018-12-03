de35826285a6f7671441ad4411272d84f62838c7
tiandeyang52 <tiandeyang52@163.com>
2018-12-03 12:51:03 +0800

    classLoader

---
+++ src/main/java/com/dytian/classloader/ClassLoaderTest.java
@@ -0,0 +1,23 @@
+package com.dytian.classloader;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
+public class ClassLoaderTest {
+
+    public static void main(String[] args) throws IOException {
+
+        String name = "java/sql/Array.class";
+        Enumeration<URL> urls = Thread.currentThread().getContextClassLoader().getResources(name);
+        while (urls.hasMoreElements()){
+            URL url = urls.nextElement();
+            System.out.println(url.toString());
+        }
+
+
+
+    }
+
+
+}
