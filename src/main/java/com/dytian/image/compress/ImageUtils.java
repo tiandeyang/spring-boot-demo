d7d1b61fb63d94b6da012d4b3423b83e522a8d96
tiandeyang52 <tiandeyang52@163.com>
2019-02-01 15:48:13 +0800

    websocket 服务器

--- src/main/java/com/dytian/image/compress/ImageUtils.java
+++ src/main/java/com/dytian/image/compress/ImageUtils.java
@@ -1,7 +1,10 @@
 package com.dytian.image.compress;
 
 import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.geometry.Positions;
 
+import javax.imageio.ImageIO;
+import java.io.File;
 import java.io.IOException;
 
 public class ImageUtils {
@@ -15,15 +18,23 @@ public class ImageUtils {
      */
 
 
+
+
     public static void main(String[] args) throws IOException {
 
-//        Thumbnails.of("D:\\jidan.png")
+
+//        Thumbnails.of("D:\\logo.jpg")
 //                .scale(1f)
 //                .outputQuality(0.5f)
-//                .toFile("D:\\jidan2.png");
+//                .toFile("D:\\logo.jpg");
+
+    //    Thumbnails.of("D:\\jidan.png").scale(1f).outputFormat("jpg").toFile("D:\\jidan4.jpg");
 
 
-        Thumbnails.of("D:\\jidan.png").scale(1f).outputFormat("jpg").toFile("D:\\jidan4.jpg");
+        //  同样的商品图片 一张有水印 一张没有水印 消费者会选择哪个商品下单
+        Thumbnails.of("D:\\my.jpg").scale(1f).watermark(Positions.CENTER, ImageIO.read(new File("D:\\share.png")), 0.3f)
+                .outputQuality(0.8f).toFile("D:\\logo_with_watermark5.jpg");
+
 
 
     }
--- src/main/java/com/dytian/netty/Server.java
+++ src/main/java/com/dytian/netty/Server.java
@@ -4,7 +4,12 @@ package com.dytian.netty;
 public class Server {
 
 
-    public void start(){
+    public static void main(String[] args) {
+
+        String url = System.getProperty("url", "localhost");
+        System.out.println(url);
+
+
 
     }
 
