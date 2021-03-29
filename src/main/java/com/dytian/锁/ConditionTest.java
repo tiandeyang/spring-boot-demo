463897d84dbb5aa55d3199259f0efac9d7f9c5ae
tiandeyang <tiandeyang52@163.com>
2021-03-29 14:42:14 +0800

    @Opt 优化

--- src/main/java/com/dytian/锁/ConditionTest.java
+++ src/main/java/com/dytian/锁/ConditionTest.java
@@ -7,21 +7,22 @@ public class ConditionTest {
 
     // Condition接口在使用前必须先调用ReentrantLock的lock()方法获得锁。之后调用Condition接口的await()将释放锁,并且在该Condition上等待,
     // 直到有其他线程调用Condition的signal()方法唤醒线程。使用方式和wait,notify类似。
-
     static ReentrantLock lock = new ReentrantLock();
     static Condition condition = lock.newCondition();
-
-
 
     public static void main(String[] args) throws InterruptedException {
 
         lock.lock();
         new Thread(new SignalThread()).start();
         System.out.println( Thread.currentThread().getName() + "主线程等待通知");
+
         try {
+            System.out.println("主线程释放锁...........waiting.........");
             condition.await();
+            System.out.println("主线程结束waiting.......往下执行....");
         } finally {
             lock.unlock();
+            System.out.println("主线程释放锁............");
         }
         System.out.println( Thread.currentThread().getName()+ "   主线程恢复运行");
     }
@@ -30,8 +31,8 @@ public class ConditionTest {
         @Override
         public void run() {
             lock.lock();
+            System.out.println("子线程获得锁...........");
             try {
-
                 try {
                     Thread.currentThread().sleep(5000);
                 } catch (InterruptedException e) {
